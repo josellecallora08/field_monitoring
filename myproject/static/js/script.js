892b4c51dcbaf46450f661ab09cc5a4e9fd422e7
soj <josellecallora08@gmail.com>
2024-04-23 12:37:29 +0800

    added filter ui

--- myproject/static/js/script.js
+++ myproject/static/js/script.js
@@ -2,6 +2,18 @@ const btnTable = document.getElementById('btn-table')
 const tableData = document.getElementById('table-data')
 const arrowBtn = document.getElementById('arrow-table')
 const chart = document.getElementById('chartContainer')
+const filterBtn = document.getElementById('filter-btn')
+const filterTbl = document.getElementById('filter-table')
+
+filterBtn.addEventListener('click', () => {
+  filterTbl.classList.toggle('hidden')
+})
+
+filterTbl.addEventListener('click', (e) => {
+  if (e.target === filterTbl) {
+    filterTbl.classList.add('hidden')
+  }
+})
 
 btnTable.addEventListener('click', () => {
     tableData.classList.toggle('-translate-x-full')
@@ -9,4 +21,5 @@ btnTable.addEventListener('click', () => {
     chart.classList.toggle('-z-50')
     console.log('hello')
 })
+
 
